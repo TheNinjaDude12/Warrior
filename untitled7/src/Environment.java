f6745db287e0bf8ff2dba97ffab0152303e7418e
TheNinjaDude12 <108714992+TheNinjaDude12@users.noreply.github.com>
2025-06-21 12:32:03 +0800

    Update Environment.java

--- untitled7/src/Environment.java
+++ untitled7/src/Environment.java
@@ -1,36 +1,72 @@
+/**
+ * Represents a battle environment that applies specific effects to combatants during battle.
+ * Different environments provide various buffs, debuffs, or neutral conditions.
+ */
 public class Environment {
-    // Store the name of the current environment
+    /**
+     * The name of the current environment.
+     */
     private String environmentName;
 
-    // Setter method to assign the environment name
-   public Environment(String environmentName) {
-       this.environmentName = environmentName;
-   }
+    /**
+     * Constructs an Environment with the specified name.
+     *
+     * @param environmentName The name of the environment.
+     */
+    public Environment(String environmentName) {
+        this.environmentName = environmentName;
+    }
 
-    // Method to apply environment-specific effects to warrior and opponent
+    /**
+     * Applies environment-specific effects to the warrior and opponent each turn.
+     * Different environments have different effects:
+     * - Swamp: Increases opponent's attack by 1 and damages warrior by 1 HP each turn
+     * - Colosseum: Increases warrior's attack by 1 and reduces opponent's defense by 1 each turn
+     * - Arena: No effects (neutral environment)
+     *
+     * @param warrior The warrior participating in combat.
+     * @param opponent The opponent participating in combat.
+     * @param turn The current turn number (currently not used in calculations but kept for future expansion).
+     */
     public void environmentEffects(Warrior warrior, Opponent opponent, int turn) {
         // Check if current environment is Swamp
         if (environmentName.equals("Swamp")) {
-            // Calculate damage modifier (currently always equals 1 due to turn + turn - turn = turn)
-            int addedDmg = turn + turn - turn;
-            // Increase opponent's attack power
-            opponent.setAttack(opponent.getAttack() + addedDmg);
+            // Apply swamp effects: favors the opponent
+            int attackBonus = 1; // Simplified from original turn calculation
+            opponent.setAttack(opponent.getAttack() + attackBonus);
             System.out.println("Opponent ATK increased by 1!");
-            // Reduce warrior's hit points by 1
+            
             warrior.setHitPoints(warrior.getHitPoints() - 1);
             System.out.println("Warrior takes 1 DMG!");
         }
         // Check if current environment is Colosseum
         else if (environmentName.equals("Colosseum")) {
-            // Calculate damage modifier (currently always equals 1 due to turn + turn - turn = turn)
-            int addedDmg = turn + turn - turn;
+            // Apply colosseum effects: favors the warrior
+            int attackBonus = 1; // Simplified from original turn calculation
             System.out.println("Warrior ATK increased by 1!");
-            // Increase warrior's attack power
-            warrior.setAttack(warrior.getAttack() + addedDmg);
+            warrior.setAttack(warrior.getAttack() + attackBonus);
+            
             System.out.println("Opponent DEF reduced by 1!");
-            // Reduce opponent's defense by 1
             opponent.setDefense(opponent.getDefense() - 1);
         }
-        // Note: No effects applied if environment is neither Swamp nor Colosseum
+        // Arena environment has no effects (neutral battlefield)
+    }
+
+    /**
+     * Retrieves the name of the current environment.
+     *
+     * @return The name of the environment.
+     */
+    public String getEnvironmentName() {
+        return environmentName;
+    }
+
+    /**
+     * Sets the name of the environment.
+     *
+     * @param environmentName The name to set for the environment.
+     */
+    public void setEnvironmentName(String environmentName) {
+        this.environmentName = environmentName;
     }
 }
